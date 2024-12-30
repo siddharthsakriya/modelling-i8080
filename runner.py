e2fab6be5b1f9b3c5c545b15b8b3b07beac7e21b
Siddharth Sakriya <siddharthsakriya@Siddharths-MacBook-Pro.local>
2024-12-30 21:30:55 +0000

    passing tst8080 :))))

--- runner.py
+++ runner.py
@@ -30,7 +30,7 @@ def read_com_file(file_path):
         print(f"An error occurred: {e}")
 
 
-file_path = '8080EXM.COM'
+file_path = 'TST8080.COM'
 read_com_file(file_path)
 commands.append("main()")
 commands.append(":run")
