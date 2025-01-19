cdeb70bb217442a0315403abf13e836594293650
Siddharth Sakriya <siddharthsakriya@Siddharths-MBP.broadband>
2025-01-19 15:26:07 +0000

    inr tests done + fixed test harness

--- testing/unit/single_reg_instructions/single_reg_tests.py
+++ testing/unit/single_reg_instructions/single_reg_tests.py
@@ -23,122 +23,106 @@ def cleanup():
     if os.path.exists(commands_file_path):
         os.remove(commands_file_path)
 
-### INR ###
-def test_inr_basic(setup_and_cleanup):
+
+def extract_results(output):
+    res = []
+    for i in range(len(output) - 3, len(output) - 15, -1):
+        res.append(output[i])
+    res.reverse()
+    register_dict = {item.split(': ')[0].replace(' Reg', ''): item.split(': ')[1] for item in res}
+    return register_dict
+
+"""INR tests"""
+
+#basic test
+def test_inr_basic():
     commands = retrieve_commands('inr')
-    commands.extend([
-        "write_mem(0x0001, 0x76)",
-        ":run", 
-        "write_reg8(0b000, 0b00000001)",
-        ":run",
-        "startup_test()", 
-        ":run", 
-        "main()", 
-        ":run", 
-        "print_b_reg()", 
-        ":run", 
-        ":quit"
-    ])
-    
+    commands.extend(["write_mem(0x0001, 0x76)", ":run", "write_reg8(0b000, 0b00000001)", ":run", "startup_test()", ":run", "main()", ":run", "print_test()", ":run", ":quit"])
+
     with open(commands_file_path, 'w') as file:
         for command in commands:
             file.write(f"{command}\n")    
     
     try:
-        result = subprocess.run(
-            ['sail', '-is', 'commands.txt', 'main.sail'],  # Use relative paths to the working directory
-            cwd=three_dirs_back, 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE, 
-            text=True,              
-            check=True              
-        )
-        output = result.stdout
-        result = re.search(r'0x[0-9a-fA-F]+', output.split('\n')[-3]).group(0)
-        assert result == "0x02" 
+        result = subprocess.run(['sail', '-is', 'commands.txt', 'main.sail'], cwd=three_dirs_back, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        output = result.stdout.split('\n')
+        results = extract_results(output)
+        assert results['B'] == '0x02'
+
     except subprocess.CalledProcessError as e:
         pytest.fail(f"Subprocess failed with error: {e}")
 
-def test_inr_zero(setup_and_cleanup):
+#parity test
+def test_inr_parity():
     commands = retrieve_commands('inr')
-    commands.extend([
-        "write_mem(0x0001, 0x76)",
-        ":run", 
-        "write_reg8(0b000, 0b11111111)",
-        ":run",
-        "startup_test()", 
-        ":run", 
-        "main()", 
-        ":run", 
-        "print_b_reg()", 
-        ":run", 
-        "print_zero_bit()",
-        ":run",
-        ":quit"
-    ])
-    
+    commands.extend(["write_mem(0x0001, 0x76)", ":run", "write_reg8(0b000, 0b00000010)", ":run", "startup_test()", ":run", "main()", ":run", "print_test()", ":run", ":quit"])
+
     with open(commands_file_path, 'w') as file:
         for command in commands:
             file.write(f"{command}\n")    
     
     try:
-        result = subprocess.run(
-            ['sail', '-is', 'commands.txt', 'main.sail'],  # Use relative paths to the working directory
-            cwd=three_dirs_back, 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE, 
-            text=True,              
-            check=True              
-        )
-        output = result.stdout
-        result = re.search(r'0x[0-9a-fA-F]+', output.split('\n')[-6]).group(0)
-        zero_flag = re.search(r'b(\d+)$', output.split('\n')[-3]).group(1)
-        assert result == "0x00" 
-        assert zero_flag == "1"
+        result = subprocess.run(['sail', '-is', 'commands.txt', 'main.sail'], cwd=three_dirs_back, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        output = result.stdout.split('\n')
+        results = extract_results(output)
+        assert results['B'] == '0x03'
+        assert results['ParityFlag'] == '0b1'
+
     except subprocess.CalledProcessError as e:
         pytest.fail(f"Subprocess failed with error: {e}")
 
-def test_inr_sign_and_parity(setup_and_cleanup):
+#auxiliary carry test
+def test_inr_ac():
     commands = retrieve_commands('inr')
-    commands.extend([
-        "write_mem(0x0001, 0x76)",
-        ":run", 
-        "write_reg8(0b000, 0b01111111)",
-        ":run",
-        "startup_test()", 
-        ":run", 
-        "main()", 
-        ":run", 
-        "print_b_reg()", 
-        ":run", 
-        "print_sign_bit()",
-        ":run",
-        "print_parity_bit()",
-        ":run",
-        ":quit"
-    ])
-    
+    commands.extend(["write_mem(0x0001, 0x76)", ":run", "write_reg8(0b000, 0b00001111)", ":run", "startup_test()", ":run", "main()", ":run", "print_test()", ":run", ":quit"])
+
     with open(commands_file_path, 'w') as file:
         for command in commands:
             file.write(f"{command}\n")    
     
     try:
-        result = subprocess.run(
-            ['sail', '-is', 'commands.txt', 'main.sail'],  # Use relative paths to the working directory
-            cwd=three_dirs_back, 
-            stdout=subprocess.PIPE, 
-            stderr=subprocess.PIPE, 
-            text=True,              
-            check=True              
-        )
-        output = result.stdout
-        result = re.search(r'0x[0-9a-fA-F]+', output.split('\n')[-9]).group(0)
-        sign_flag = re.search(r'b(\d+)$', output.split('\n')[-6]).group(1)
-        parity_flag = re.search(r'b(\d+)$', output.split('\n')[-3]).group(1)
-        assert result == "0x80" 
-        assert parity_flag == "0"
-        assert sign_flag == "1"
-    
+        result = subprocess.run(['sail', '-is', 'commands.txt', 'main.sail'], cwd=three_dirs_back, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        output = result.stdout.split('\n')
+        results = extract_results(output)
+        assert results['B'] == '0x10'
+        assert results['AuxCarryFlag'] == '0b1'
+
     except subprocess.CalledProcessError as e:
         pytest.fail(f"Subprocess failed with error: {e}")
 
+#zero flag test
+def test_inr_zero():
+    commands = retrieve_commands('inr')
+    commands.extend(["write_mem(0x0001, 0x76)", ":run", "write_reg8(0b000, 0b11111111)", ":run", "startup_test()", ":run", "main()", ":run", "print_test()", ":run", ":quit"])
+
+    with open(commands_file_path, 'w') as file:
+        for command in commands:
+            file.write(f"{command}\n")    
+    
+    try:
+        result = subprocess.run(['sail', '-is', 'commands.txt', 'main.sail'], cwd=three_dirs_back, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        output = result.stdout.split('\n')
+        results = extract_results(output)
+        assert results['B'] == '0x00'
+        assert results['ZeroFlag'] == '0b1'
+
+    except subprocess.CalledProcessError as e:
+        pytest.fail(f"Subprocess failed with error: {e}")
+
+def test_inr_sign():
+    commands = retrieve_commands('inr')
+    commands.extend(["write_mem(0x0001, 0x76)", ":run", "write_reg8(0b000, 0b01111111)", ":run", "startup_test()", ":run", "main()", ":run", "print_test()", ":run", ":quit"])
+
+    with open(commands_file_path, 'w') as file:
+        for command in commands:
+            file.write(f"{command}\n")    
+    
+    try:
+        result = subprocess.run(['sail', '-is', 'commands.txt', 'main.sail'], cwd=three_dirs_back, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
+        output = result.stdout.split('\n')
+        results = extract_results(output)
+        assert results['B'] == '0x80'
+        assert results['SignFlag'] == '0b1'
+
+    except subprocess.CalledProcessError as e:
+        pytest.fail(f"Subprocess failed with error: {e}")    
